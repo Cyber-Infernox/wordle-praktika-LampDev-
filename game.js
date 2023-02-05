9152916cb11de3bfa5e530e9671431aa41e71162
zheni4ka <89845641+zheni4ka@users.noreply.github.com>
2023-02-05 18:32:43 +0300

    Update game.js

--- game.js
+++ game.js
@@ -14,18 +14,9 @@ $(document).ready(async function () {
         $('#gamesplayed').text(gamesplayed);
         $('#wordsguessed').text(wordsguessed);
     });
-    $('#login').click(function () {
-        $('#loginmodal').fadeIn(150);
-        $('#highscore').text(highscore);
-        $('#gamesplayed').text(gamesplayed);
-        $('#wordsguessed').text(wordsguessed);
-    });
 
     $('#closescore').click(function () {
         $('#scoremodal').fadeOut(150);
-    });
-    $('#loginscore').click(function () {
-        $('#loginmodal').fadeOut(150);
     });
 
     $('#closegameover').click(function () {
@@ -170,7 +161,7 @@ $(document).ready(async function () {
         guess = '';
         isgameover = false;
         attempthistory = [];
-        $('#title').text(`Wordler`);
+        $('#title').text(`Wordle`);
         $('#title').attr('class', 'text-5xl font-extrabold my-auto');
         newword();
         save();
@@ -372,64 +363,4 @@ $(document).ready(async function () {
     }
 });
 
-$(document).ready(function() {
-    $("#do_login").click(function() { 
-       closeLoginInfo();
-       $(this).parent().find('span').css("display","none");
-       $(this).parent().find('span').removeClass("i-save");
-       $(this).parent().find('span').removeClass("i-warning");
-       $(this).parent().find('span').removeClass("i-close");
-       
-        var proceed = true;
-        $("#login_form input").each(function(){
-            
-            if(!$.trim($(this).val())){
-                $(this).parent().find('span').addClass("i-warning");
-                $(this).parent().find('span').css("display","block");  
-                proceed = false;
-            }
-        });
-       
-        if(proceed) //everything looks good! proceed...
-        {
-            $(this).parent().find('span').addClass("i-save");
-            $(this).parent().find('span').css("display","block");
-        }
-    });
-    
-    //reset previously results and hide all message on .keyup()
-    $("#login_form input").keyup(function() { 
-        $(this).parent().find('span').css("display","none");
-    });
- 
-  openLoginInfo();
-  setTimeout(closeLoginInfo, 1000);
-});
 
-function openLoginInfo() {
-    $(document).ready(function(){ 
-        $('.b-form').css("opacity","0.01");
-      $('.box-form').css("left","-37%");
-      $('.box-info').css("right","-37%");
-    });
-}
-
-function closeLoginInfo() {
-    $(document).ready(function(){ 
-        $('.b-form').css("opacity","1");
-        $('.box-form').css("left","0px");
-      $('.box-info').css("right","-5px"); 
-    });
-}
-
-$(window).on('resize', function(){
-      closeLoginInfo();
-});
-const switchers = [...document.querySelectorAll('.switcher')]
-
-switchers.forEach(item => {
-    item.addEventListener('click', function() {
-        switchers.forEach(item => item.parentElement.classList.remove('is-active'))
-        this.parentElement.classList.add('is-active')
-    })
-})
